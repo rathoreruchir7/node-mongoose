94c7960b2b9f88a056e2e7b6acd4bcb90068e48d
Ruchir Rathore <rathoreruchir7@gmail.com>
2020-06-17 21:13:07 +0530

    Mongoose Part 3

--- index.js
+++ index.js
@@ -8,26 +8,44 @@ const connect = mongoose.connect(url);
 connect.then((db) => {
 
     console.log('Connected correctly to server');
-    
-    var newDish = Dishes.create({
-        name: 'Uthappizza1',
+    Dishes.create({
+        name: 'Uthappizza6',
         description: 'test'
     })
-        .then((dish) => {
-            console.log(dish);
-
-            return Dishes.find({});
+    .then((dish) => {
+        console.log(dish);
+       
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test'}
+        },{ 
+            new: true 
         })
-        .then((dishes) => {
-            console.log(dishes);
-
-            return Dishes.remove({});
-        })
-        .then(() => {
-            return mongoose.connection.close();
-        })
-        .catch((err) => {
-            console.log(err);
+        .exec();
+        
+    })
+    .then((dish) => {
+        console.log(dish);
+     
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
         });
+
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
+        console.log(dish.comments[0]);
+
+        return Dishes.remove({});
+    })
+    .then(() => {
+        
+        return mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 });
